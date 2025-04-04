f59138d88721122326c5f6b810a60718a7b31c66
이상현 <firstquarterlee@gmail.com>
2025-04-04 17:21:31 +0900

    04/04 Study

---
+++ 3.JS/10.showhide.js
@@ -0,0 +1,9 @@
+function showhide() {
+  var mydiv = document.getElementById("showhide");
+  if (mydiv.style.display === "none") {
+    mydiv.style.display = "block";
+  } else {
+    mydiv.style.display = "none";
+  }
+  // 미션1. 이 요소를 숨기기 (버튼 다시 누르면 보이기)
+}
