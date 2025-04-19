6f20f83f02b0e3c13f1a1e31861f5aa9de5d25a9
이상현 <firstquarterlee@gmail.com>
2025-04-19 14:39:23 +0900

    feat: python 300제 20/300

--- 4.Python/0.python_300prob/1-300prob.py
+++ 4.Python/0.python_300prob/1-300prob.py
@@ -47,5 +47,74 @@ print("======================")
 #010번
 print(5/3)
 
+print("======================")
+
 #011번
-print()
+samsungElectronics = 5000
+myStock = samsungElectronics * 10
+print(myStock)
+
+print("======================")
+
+#012번
+marketCapitalization = 298000000000000
+currentPrice = 50000
+per = 15.79
+
+print(marketCapitalization, type(marketCapitalization))
+print(currentPrice, type(currentPrice))
+print(per, type(per))
+
+print("======================")
+
+#013번
+s = "hello"
+t = "python"
+print(f"{s}! {t}")
+
+print("======================")
+
+#014번
+#8
+
+print("======================")
+
+#015번
+a = "132"
+print(a, type(a))
+
+print("======================")
+
+#016번
+num_str = "720"
+changeNumber = int(num_str)
+print(changeNumber, type(changeNumber))
+
+print("======================")
+
+#017번
+num = 100
+print(str(num))
+
+print("======================")
+#018번
+changeStr = "15.79"
+data = float(changeStr)
+print(data, type(data))
+
+print("======================")
+#019번
+year = "2020"
+data = int(year)
+print(data -3) # 2017
+print(data -2) # 2018
+print(data -1) # 2019
+
+print("======================")
+#020번
+month = 48584
+muiza = 36
+print(month * muiza)
+
+print("======================")
+
